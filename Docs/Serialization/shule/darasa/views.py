c3e70d8b7db20cbf0a13290333706024a9414bce
BikoCodes <bikocodes@gmail.com>
2021-09-23 14:01:51 +0300

    Generics Class Based Views

--- Docs/Serialization/shule/darasa/views.py
+++ Docs/Serialization/shule/darasa/views.py
@@ -109,35 +109,43 @@ from rest_framework import generics
 #         darasa.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class DarasaList(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
+# class DarasaList(mixins.ListModelMixin,
+#                 mixins.CreateModelMixin,
+#                 generics.GenericAPIView):
     
+#     queryset = Darasa.objects.all()
+#     serializer_class = DarasaSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+# class DarasaDetail(mixins.RetrieveModelMixin,
+#                     mixins.UpdateModelMixin,
+#                     mixins.DestroyModelMixin,
+#                     generics.GenericAPIView):
+    
+#     queryset = Darasa.objects.all()
+#     serializer_class = DarasaSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)    
+
+
+class DarasaList(generics.ListCreateAPIView):
     queryset = Darasa.objects.all()
     serializer_class = DarasaSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class DarasaDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    
+class DarasaDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Darasa.objects.all()
-    serializer_class = DarasaSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)    
-
-
+    serializer_class = Darasa.DarasaSerializer
+    
