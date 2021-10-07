a963c9fe99a708ac2f5083ca29e5706a9837f798
BikoCodes <bikocodes@gmail.com>
2021-10-07 15:58:11 +0300

    Restricting Access With Permissions

--- Mitch/permissions/blog/api/views.py
+++ Mitch/permissions/blog/api/views.py
@@ -1,16 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 
 from account.models import Account
 from blog.models import BlogPost
 from blog.api.serializers import BlogPostSerializer
 
 
-
 @api_view(['GET',])
-@permission_classes([IsAuthenticated])
 def api_detail_blog_view(request, slug):
 
     try:
@@ -24,17 +21,12 @@ def api_detail_blog_view(request, slug):
 
 
 @api_view(['PUT',])
-@permission_classes([IsAuthenticated])
 def api_update_blog_view(request, slug):
 
     try:
         blog_post = BlogPost.objects.get(slug=slug)
     except BlogPost.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-    user = request.user
-    if blog_post.author != user:
-        return Response({'response': "You don't have permission to edit that."})
 
     if request.method == 'PUT':
         serializer = BlogPostSerializer(blog_post, data=request.data)
@@ -49,17 +41,12 @@ def api_update_blog_view(request, slug):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE',])
-@permission_classes([IsAuthenticated])
 def api_delete_blog_view(request, slug):
 
     try:
         blog_post = BlogPost.objects.get(slug=slug)
     except BlogPost.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-    user = request.user
-    if blog_post.author != user:
-        return Response({'response': "You don't have permission to delete that."})
 
     if request.method == 'DELETE':
         operation = blog_post.delete()
@@ -73,10 +60,9 @@ def api_delete_blog_view(request, slug):
         return Response(data=data)
 
 @api_view(['POST',])
-@permission_classes([IsAuthenticated])
 def api_create_blog_view(request, slug):
 
-    account = request.user
+    account = Account.objects.get(pk=1)
 
     blog_post = BlogPost(author=account)
 
