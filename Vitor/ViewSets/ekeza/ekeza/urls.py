177067e572eecef97974dc550909f9ba9f456b2e
BikoCodes <bikocodes@gmail.com>
2021-10-13 16:34:43 +0300

    ViewSets and Pagination

---
+++ Vitor/ViewSets/ekeza/ekeza/urls.py
@@ -0,0 +1,15 @@
+# from django.contrib import admin
+from django.urls import path
+from core import views
+
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+
+router.register(r'categories', views.CategoryModelViewSet, basename='category')
+router.register(r'transactions', views.TransactionModelViewSet, basename='transaction')
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path("currencies/", views.CurrencyListAPIView.as_view(), name="currencies")
+] + router.urls
